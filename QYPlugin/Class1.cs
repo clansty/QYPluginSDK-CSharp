8cb188987bd10af2d39ecd4221c8b2dbfef1c119
凌莞 <i@gao4.pw>
2019-04-03 20:58:16 +0800

    取应用数据目录

--- QYPlugin/Class1.cs
+++ QYPlugin/Class1.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QYPlugin
 {
-    public class Class1
+    public static class Robot
     {
+        private static int AuthCode;
+
+        /// <summary>
+        /// 获取应用数据目录
+        /// <para>返回如：D:\robot\plugin\com.contract.testapp\</para>
+        /// <para>应用的所有数据、配置【必须】存放于此目录，避免给用户带来困扰。</para>
+        /// </summary>
+        public static string AppDirectory => Marshal.PtrToStringAnsi(QY_getAppDirectory(AuthCode));
+        [DllImport("QYOffer.dll")]
+        private static extern IntPtr QY_getAppDirectory(int authCode);
+
+
     }
 }
